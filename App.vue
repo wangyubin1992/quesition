1d8640187d72318f6426a9c86b0331c3d51bcaf3
wangyubin1992 <14747128939@163.com>
2017-11-16 17:34:57 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    
+    <loading v-model="isLoading"></loading>
+  </div>
+</template>
+
+<script>
+import { Loading } from 'vux'
+import { mapState } from 'Vuex'
+import format from './assets/js/setRem'
+
+export default {
+  name: 'App',
+  components: {
+    Loading
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.vux.isLoading
+    })
+  },
+  created: function (){
+  	format.setRem()
+  }
+}
+</script>
+
+<style lang="less">
+@import '~vux/src/styles/reset.less';
+@import '~vux/src/styles/1px.less';
+@import '~vux/src/styles/close.less';
+body {
+  background-color: #F9F9F9;
+}
+</style>
